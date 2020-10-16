2d23f2c7d85584b12ac7245ebdbf5ef504a63078
valentinegrand <72909592+valentinegrand@users.noreply.github.com>
2020-10-16 13:06:56 +0200

    Add files via upload

--- crud.py
+++ crud.py
@@ -3,7 +3,7 @@ file crud.py
 manage CRUD and adapt model data from db to schema data to api rest
 """
 
-
+import pandas as pd
 from typing import Optional,List
 from sqlalchemy.orm import Session
 from sqlalchemy import desc, extract, between
@@ -12,6 +12,12 @@ from fastapi.logger import logger
 import models, schemas
 
 # CRUD for Movie objects
+
+#----------------------------------------- MOVIES ---------------------------------------------
+
+########## Fonctions GET 
+
+# Récupérer un movie grâce à son ID 
 def get_movie(db: Session, movie_id: int):
     # read from the database (get method read from cache)
     # return object read or None if not found
@@ -22,19 +28,25 @@ def get_movie(db: Session, movie_id: int):
     logger.error(f"actors: {db_movie.actors}")
     return db_movie
 
+# Récupération de tous les films (limite à 100)
 def get_movies(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Movie).offset(skip).limit(limit).all()
 
+# Récupération des films avec le titre (en entier)
 def get_movies_by_title(db: Session, title: str):
     return db.query(models.Movie).filter(models.Movie.title == title).all()
 
-def get_movies_by_year(db: Session, year: int):
-    return db.query(models.Movie).filter(models.Movie.year == year).all()
-
+# Récupération des films avec seulement une partie du titre
 def get_movies_by_parttitle(db: Session, title: str):
     return db.query(models.Movie).filter(models.Movie.title.like(f'%{title}%')).all()
 
 
+# Récupération des films avec l'année
+def get_movies_by_year(db: Session, year: int):
+    return db.query(models.Movie).filter(models.Movie.year == year).all()
+
+
+# Récupération des films avec un intervalle d'années
 def get_movies_by_range_year(db: Session, year_min: Optional[int] = None, year_max: Optional[int] = None):
     if year_min is None and year_max is None:
         return None
@@ -51,20 +63,16 @@ def get_movies_by_range_year(db: Session, year_min: Optional[int] = None, year_m
                 .all()
 
 
-
+# Récupération des films avec le nom du réalisateur 
 def get_movies_by_director_endname(db: Session, endname: str):
     return db.query(models.Movie).join(models.Movie.director)      \
             .filter(models.Star.name.like(f'%{endname}')) \
             .order_by(desc(models.Movie.year))  \
             .all()
 
-def get_movies_by_actor(db: Session, name: str):
-    return db.query(models.Movie).join(models.Movie.actors) \
-            .filter(models.Star.name.like(f'%{name}'))   \
-            .order_by(desc(models.Movie.year))              \
-            .all()
 
 
+# Nombre de films par année
 def get_movies_count_by_year(db: Session):
     # NB: func.count() i.e. count(*) en SQL
     return db.query(models.Movie.year, func.count())  \
@@ -72,6 +80,7 @@ def get_movies_count_by_year(db: Session):
             .order_by(models.Movie.year)  \
             .all()
 
+# Durée du film min, max et moyenne pour chaque année
 def get_movies_duration_by_year(db: Session):
     # NB: func.count() i.e. count(*) en SQL
     return db.query(models.Movie.year, func.min(models.Movie.duration),func.max(models.Movie.duration),func.avg(models.Movie.duration))  \
@@ -79,19 +88,174 @@ def get_movies_duration_by_year(db: Session):
             .order_by(models.Movie.year)  \
             .all()
 
+########## Fonction add/create : POST
 
-def update_movie_director(db: Session, movie_id: int, director_id: int):
-    db_movie = get_movie(db=db, movie_id=movie_id)
-    db_star =  get_star(db=db, star_id=director_id)
-    if db_movie is None or db_star is None:
-        return None
-    # update object association
-    db_movie.director = db_star
-    # commit transaction : update SQL
+# Créer un film et l'ajouter à la bdd
+def create_movie(db: Session, movie: schemas.MovieCreate):
+    # convert schema object from rest api to db model object
+    db_movie = models.Movie(title=movie.title, year=movie.year, duration=movie.duration)
+    # add in db cache and force insert
+    db.add(db_movie)
     db.commit()
-    # return updated object
+    # retreive object from db (to read at least generated id)
+    db.refresh(db_movie)
     return db_movie
 
+########## Fonction update : PUT
+
+# Modifier un film et aplliquer dans la bdd
+def update_movie(db: Session, movie: schemas.Movie):
+    db_movie = db.query(models.Movie).filter(models.Movie.id == movie.id).first()
+    if db_movie is None:
+        return None
+        # update data from db
+        db_movie.title = movie.title
+        db_movie.year = movie.year
+        db_movie.duration = movie.duration
+        # validate update in db
+        db.commit()
+    # return updated object or None if not found
+    return db_movie
+
+########## Fonction delete : DELETE
+
+#Supprimer un film dans la bdd
+def delete_movie(db: Session, movie_id: int):
+     db_movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+     if db_movie is None:
+         return None
+         # delete object from ORM
+         db.delete(db_movie)
+         # validate delete in db
+         db.commit()
+     # return deleted object or None if not found
+     return db_movie
+
+
+#----------------------------------------- STARS ---------------------------------------------
+
+########## Fonctions GET 
+
+#Récupérer une star avec son id 
+def get_star(db: Session, star_id: int):
+    # read from the database (get method read from cache)
+    # return object read or None if not found
+    return db.query(models.Star).filter(models.Star.id == star_id).first()
+    #return db.query(models.Star).get(1)
+    #return schemas.Star(id=1, name="Fred")
+
+# Récupérer toutes les stars (limite de 100)
+def get_stars(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Star).offset(skip).limit(limit).all()
+
+# Récupérer une star avec son nom (en entier)
+def get_stars_by_name(db: Session, name: str):
+    return db.query(models.Star).filter(models.Star.name == name).order_by(models.Star.name).all()
+
+# Récupérer une star avec son nom (peut être partiellement écrit)
+def get_stars_by_partname(db: Session, name: str):
+    return db.query(models.Star).filter(models.Star.name.like(f'%{name}%')).all()
+
+# Récupérer une star avec sa date de naissance
+def get_stars_by_birthyear(db: Session, year: int):
+    return db.query(models.Star).filter(extract('year', models.Star.birthdate) == year) \
+            .order_by(models.Star.name)  \
+            .all()
+######### Fonction add/create : POST
+
+# AJouter une star à la bdd
+def create_star(db: Session, star: schemas.StarCreate):
+    # convert schema object from rest api to db model object
+    db_star = models.Star(name=star.name, birthdate=star.birthdate)
+    # add in db cache and force insert
+    db.add(db_star)
+    db.commit()
+    # retreive object from db (to read at least generated id)
+    db.refresh(db_star)
+    return db_star
+
+########## Fonction update : PUT
+
+# Modifier une star de la bdd
+def update_star(db: Session, star: schemas.Star):
+    db_star = db.query(models.Star).filter(models.Star.id == star.id).first()
+        # update data from db
+    db_star.name = star.name
+    db_star.birthdate = star.birthdate
+        # validate update in db
+    db.commit()
+    # return updated object or None if not found
+    return db_star
+
+########## Fonction delete : DELETE   
+
+# Supprimer une star de la BDD
+def delete_star(db: Session, star_id: int):
+     db_star = db.query(models.Star).filter(models.Star.id == star_id).first()
+     if db_star is None:
+         return None
+         # delete object from ORM
+         db.delete(db_star)
+         # validate delete in db
+         db.commit()
+     # return deleted object or None if not found
+     return db_star
+
+
+#----------------------------------------- ASSOCIATION PLAY ---------------------------------------------
+
+
+######### Fonction GET : GET  (+stats)
+
+# Récupérer le réalisateur avec l'id du film
+def get_stars_by_movie_id(db: Session, movie_id: Optional[int] = None):
+    movie_director = db.query(models.Movie).filter(models.Movie.id == movie_id).join(models.Movie.director).first()
+    if movie_director is not None:
+        return movie_director.director
+    else:
+        return None
+
+# Récupérer le réalisateur avec le titre (partiel ou non) du film
+def get_star_director_movie_by_title(db: Session, title: str):
+    db_movies = db.query(models.Movie).filter(models.Movie.title.like(f'%{title}%')) \
+        .join(models.Movie.director)  
+    return [ db_movie.director for db_movie in db_movies ]
+
+# Récupérer le nom des acteurs d'un film avec son titre (partiel ou non)
+def get_actors_by_movie_title(db: Session, title: str):
+    return db.query(models.Star).join(models.Movie.actors) \
+            .filter(models.Movie.title.like(f'%{title}'))   \
+            .order_by(models.Star.name) \
+            .all()
+
+
+# Récupération du film avec le nom d'un acteur
+def get_movies_by_actor(db: Session, name: str):
+    return db.query(models.Movie).join(models.Movie.actors) \
+            .filter(models.Star.name.like(f'%{name}'))   \
+            .order_by(desc(models.Movie.year))              \
+            .all()
+
+# Nombre de films par réalisateur (avec nb de films supérieur à ...)
+def get_stats_by_movie_director(db: Session, min_count: int):
+    return db.query(models.Star, func.count(models.Movie.id).label("movie_count"))  \
+        .join(models.Movie.director)        \
+        .group_by(models.Star)  \
+        .having(func.count(models.Movie.id) >= min_count) \
+        .order_by(desc("movie_count")) \
+        .all()
+
+def get_stats_by_movie_actor(db: Session, min_count: int):
+    return db.query(models.Star, func.count(models.Movie.id).label("movie_count"),func.min(models.Movie.year).label("first_year_movie"),func.max(models.Movie.year).label("last_year_movie")) \
+        .join(models.Movie.actors)        \
+        .group_by(models.Star)  \
+        .having(func.count(models.Movie.id) >= min_count) \
+        .order_by(desc("movie_count")) \
+        .all()
+
+########## Fonction add : POST
+
+# Ajouter un acteur à un film dans la bdd
 def add_movie_actor(db: Session, movie_id: int, actor_id: int):
     db_movie = get_movie(db=db, movie_id=movie_id)
     db_star =  get_star(db=db, star_id=actor_id)
@@ -104,127 +268,45 @@ def add_movie_actor(db: Session, movie_id: int, actor_id: int):
     # return updated object
     return db_movie
 
+########## Fonction update : UPDATE
 
-def update_movie_actor(db: Session, movie_id: int, actor_id: List[int]):
+# Modifier le réalisateur d'un film
+def update_movie_director(db: Session, movie_id: int, director_id: int):
     db_movie = get_movie(db=db, movie_id=movie_id)
-    for i in len(actor_id) : 
-        db_star = db_star.append(get_star(db=db, star_id=actor_id[i]))
+    db_star =  get_star(db=db, star_id=director_id)
     if db_movie is None or db_star is None:
         return None
     # update object association
-    db_movie.actors = db_star
+    db_movie.director = db_star
     # commit transaction : update SQL
     db.commit()
     # return updated object
     return db_movie
 
 
-def create_movie(db: Session, movie: schemas.MovieCreate):
-    # convert schema object from rest api to db model object
-    db_movie = models.Movie(title=movie.title, year=movie.year, duration=movie.duration)
-    # add in db cache and force insert
-    db.add(db_movie)
+
+# Modifier la liste des acteurs d'un film
+def update_movie_actor(db: Session, movie_id: int, actor_id: List[int]):
+    db_movie = get_movie(db=db, movie_id=movie_id)
+    db_movie.actors = []
+    for i in actor_id : 
+        db_star = get_star(db=db, star_id=i)
+        if db_movie is None or db_star is None:
+            return None
+    # update object association 
+        db_movie.actors.append(db_star)
+    # commit transaction : update SQL
     db.commit()
-    # retreive object from db (to read at least generated id)
-    db.refresh(db_movie)
+    # return updated object
     return db_movie
 
 
 
-def update_movie(db: Session, movie: schemas.Movie):
-    db_movie = db.query(models.Movie).filter(models.Movie.id == movie.id).first()
-    if db_movie is not None:
-        # update data from db
-        db_movie.title = movie.title
-        db_movie.year = movie.year
-        db_movie.duration = movie.duration
-        # validate update in db
-        db.commit()
-    # return updated object or None if not found
-    return db_movie
-
-def delete_movie(db: Session, movie_id: int):
-     db_movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
-     if db_movie is not None:
-         # delete object from ORM
-         db.delete(db_movie)
-         # validate delete in db
-         db.commit()
-     # return deleted object or None if not found
-     return db_movie
 
 
 
-def get_star(db: Session, star_id: int):
-    # read from the database (get method read from cache)
-    # return object read or None if not found
-    return db.query(models.Star).filter(models.Star.id == star_id).first()
-    #return db.query(models.Star).get(1)
-    #return schemas.Star(id=1, name="Fred")
-
-def get_stars(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Star).offset(skip).limit(limit).all()
 
 
-def get_stars_by_name(db: Session, name: str):
-    return db.query(models.Star).filter(models.Star.name == name).order_by(models.Star.name).all()
 
 
-def get_stars_by_partname(db: Session, name: str):
-    return db.query(models.Star).filter(models.Star.name.like(f'%{name}%')).all()
 
-
-def get_stars_by_birthdate(db: Session, year: int):
-    return db.query(models.Star).filter(models.Star.birthdate== year).order_by(models.Star.name).all()
-
-
-def get_stars_by_movie_id(db: Session, movie_id: Optional[int] = None):
-    movie_director = db.query(models.Movie).filter(models.Movie.id == movie_id).join(models.Movie.director).first()
-    if movie_director is not None:
-        return movie_director.director
-    else:
-        return None
-
-def get_star_director_movie_by_title(db: Session, title: str):
-    db_movies = db.query(models.Movie).filter(models.Movie.title.like(f'%{title}%')) \
-        .join(models.Movie.director)  
-    return [ db_movie.director for db_movie in db_movies ]
-
-
-def get_actors_by_movie_title(db: Session, title: str):
-    return db.query(models.Star).join(models.Movie.actors) \
-            .filter(models.Movie.title.like(f'%{title}'))   \
-            .order_by(models.Star.name) \
-            .all()
-
-
-def create_star(db: Session, star: schemas.StarCreate):
-    # convert schema object from rest api to db model object
-    db_star = models.Star(name=star.name, birthdate=star.birthdate)
-    # add in db cache and force insert
-    db.add(db_star)
-    db.commit()
-    # retreive object from db (to read at least generated id)
-    db.refresh(db_star)
-    return db_star
-
-def update_star(db: Session, star: schemas.Star):
-    db_star = db.query(models.Star).filter(models.Star.id == star.id).first()
-    if db_star is not None:
-        # update data from db
-        db_star.name = star.name
-        db_star.birthdate = star.birthdate
-        # validate update in db
-        db.commit()
-    # return updated object or None if not found
-    return db_star
-
-def delete_star(db: Session, star_id: int):
-     db_star = db.query(models.Star).filter(models.Star.id == star_id).first()
-     if db_star is not None:
-         # delete object from ORM
-         db.delete(db_star)
-         # validate delete in db
-         db.commit()
-     # return deleted object or None if not found
-     return db_star
--- main.py
+++ main.py
@@ -27,33 +27,16 @@ def get_db():
     finally:
         db.close()
 
-
+'''
 @app.post("/movies/")
 def create_movie(movie: schemas.MovieCreate, db: Session = Depends(get_db)):
     return False
-
-@app.put("/movies/director", response_model=schemas.MovieDetail)
-def update_movie_director(mid: int, sid: int, db: Session = Depends(get_db)):
-    db_movie = crud.update_movie_director(db=db, movie_id=mid, director_id=sid)
-    if db_movie is None:
-        raise HTTPException(status_code=404, detail="Movie or Star not found")
-    return db_movie
-
-@app.post("/movies/actor", response_model=schemas.MovieDetail)
-def add_movie_actor(mid: int, sid: int, db: Session = Depends(get_db)):
-    db_movie = crud.add_movie_actor(db=db, movie_id=mid, actor_id=sid)
-    if db_movie is None:
-        raise HTTPException(status_code=404, detail="Movie or Star not found")
-    return db_movie
+'''
 
 
-@app.put("/movies/actors", response_model=schemas.MovieDetail)
-def update_movie_actors(mid: int, sids: List[int], db: Session = Depends(get_db)):
-    db_movie = crud.update_movie_actor(db=db, movie_id=mid, actor_id=sids)
-    if db_movie is None:
-        raise HTTPException(status_code=404, detail="Movie or Star not found")
-    return db_movie
-    pass
+#----------------------------------------- MOVIES ---------------------------------------------
+
+########## App GET 
 
 
 @app.get("/movies/", response_model=List[schemas.Movie])
@@ -70,7 +53,7 @@ def read_movie(movie_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Movie to read not found")
     return db_movie
 
-### POur pouvoir gérer les titres compliqués on ne met pas de {title} et on laisse postamn gérer les caractères : by_title?title=...
+### Pour pouvoir gérer les titres compliqués on ne met pas de {title} et on laisse postman gérer les caractères : by_title?title=...
 @app.get("/movies/by_title", response_model=List[schemas.Movie])
 def read_movies_by_title(title: str, db: Session = Depends(get_db)):
     # read items from database
@@ -107,13 +90,6 @@ def read_movies_by_range_year(year_min: Optional[int] = None,
     return movies
 
 
-@app.get("/movies/by_director", response_model=List[schemas.Movie])
-def read_movies_by_director(n: str, db: Session = Depends(get_db)):
-    return crud.get_movies_by_director_endname(db=db, endname=n)
-
-@app.get("/movies/by_actor", response_model=List[schemas.Movie])
-def read_movies_by_actor(name: str, db: Session = Depends(get_db)):
-    return crud.get_movies_by_actor(db=db, name=name)
 
 
 @app.get("/movies/count_by_year")
@@ -124,10 +100,14 @@ def read_count_movies_by_year(db: Session = Depends(get_db)) -> List[Tuple[int, 
 def read_count_movies_min_duration_by_year(db: Session = Depends(get_db)) -> List[Tuple[int, int,int,int]]:
     return crud.get_movies_duration_by_year(db=db)
 
+########## POST
+
 @app.post("/movies/", response_model=schemas.Movie)
 def create_user(movie: schemas.MovieCreate, db: Session = Depends(get_db)):
     # receive json item without id and return json item from database with new id
     return crud.create_movie(db=db, movie=movie)
+
+######### PUT
 
 @app.put("/movies/", response_model=schemas.Movie)
 def update_movie(movie: schemas.Movie, db: Session = Depends(get_db)):
@@ -136,6 +116,8 @@ def update_movie(movie: schemas.Movie, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Item to update not found")
     return db_movie
 
+######### DELETE
+
 @app.delete("/movies/{movie_id}", response_model=schemas.Movie)
 def delete_movie(movie_id: int, db: Session = Depends(get_db)):
     db_movie = crud.delete_movie(db, movie_id=movie_id)
@@ -143,6 +125,9 @@ def delete_movie(movie_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Item to delete not found")
     return db_movie
 
+#----------------------------------------- STARS ---------------------------------------------
+
+########## App GET 
 
 @app.get("/stars", response_model=List[schemas.Star])
 def read_stars(skip: Optional[int] = 0, limit: Optional[int] = 100, db: Session = Depends(get_db)):
@@ -173,12 +158,61 @@ def read_stars_by_partname(name: Optional[str] = None, db: Session = Depends(get
     # return them as json
     return star
 
-@app.get("/stars/by_birthdate/{year}", response_model=List[schemas.Star])
+@app.get("/stars/by_birthdate", response_model=List[schemas.Star])
 def read_star_by_birthyear(year: Optional[int], db: Session = Depends(get_db)):
     # read items from database
-    star = crud.get_stars_by_birthdate(db=db, year=year)
+    star = crud.get_stars_by_birthyear(db=db, year=year)
     # return them as json
     return star
+
+ 
+ 
+@app.get("/stars/stats_movie_by_director")
+def read_stats_movie_by_director(minc: Optional[int] = 10, db: Session = Depends(get_db)):
+    return crud.get_stats_by_movie_director(db=db, min_count=minc)
+
+
+@app.get("/stars/stats_movie_by_actor")
+def read_stats_movie_by_actor(minc: Optional[int] = 10, db: Session = Depends(get_db)):
+    return crud.get_stats_by_movie_actor(db=db, min_count=minc)
+
+########## App POST
+    
+@app.post("/stars/", response_model=schemas.Star)
+def create_user(star: schemas.StarCreate, db: Session = Depends(get_db)):
+    # receive json item without id and return json item from database with new id
+    return crud.create_star(db=db, star=star)
+
+######## App PUT
+
+@app.put("/stars/", response_model=schemas.Star)
+def update_star(star: schemas.Star, db: Session = Depends(get_db)):
+    db_star = crud.update_star(db, star=star)
+    if db_star is None:
+        raise HTTPException(status_code=404, detail="Item to update not found")
+    return db_star
+
+######### App DELETE
+
+@app.delete("/stars/{star_id}", response_model=schemas.Star)
+def delete_star(star_id: int, db: Session = Depends(get_db)):
+    db_star = crud.delete_star(db, star_id=star_id)
+    if db_star is None:
+        raise HTTPException(status_code=404, detail="Item to delete not found")
+    return db_star
+
+
+#----------------------------------------- ASSOCIATION PLAY ---------------------------------------------
+
+########## App GET
+
+@app.get("/movies/by_director", response_model=List[schemas.Movie])
+def read_movies_by_director(n: str, db: Session = Depends(get_db)):
+    return crud.get_movies_by_director_endname(db=db, endname=n)
+
+@app.get("/movies/by_actor", response_model=List[schemas.Movie])
+def read_movies_by_actor(name: str, db: Session = Depends(get_db)):
+    return crud.get_movies_by_actor(db=db, name=name)
 
 @app.get("/stars/by_movie_id/{movie_id}", response_model=schemas.Star)
 def read_star(movie_id: int, db: Session = Depends(get_db)):
@@ -194,25 +228,32 @@ def read_stars_by_movie_directed_title(title: str, db: Session = Depends(get_db)
 
 @app.get("/stars/actors_by_title", response_model=List[schemas.Star])
 def read_stars_by_movie_played_title(title: str, db: Session = Depends(get_db)):
-    return crud.get_actors_by_movie_title(db=db, title=title)    
- 
-
-@app.post("/stars/", response_model=schemas.Star)
-def create_user(star: schemas.StarCreate, db: Session = Depends(get_db)):
-    # receive json item without id and return json item from database with new id
-    return crud.create_star(db=db, star=star)
+    return crud.get_actors_by_movie_title(db=db, title=title)   
 
 
-@app.put("/stars/", response_model=schemas.Star)
-def update_star(star: schemas.Star, db: Session = Depends(get_db)):
-    db_star = crud.update_star(db, star=star)
-    if db_star is None:
-        raise HTTPException(status_code=404, detail="Item to update not found")
-    return db_star
+######### App POST
 
-@app.delete("/stars/{star_id}", response_model=schemas.Star)
-def delete_star(star_id: int, db: Session = Depends(get_db)):
-    db_star = crud.delete_star(db, star_id=star_id)
-    if db_star is None:
-        raise HTTPException(status_code=404, detail="Item to delete not found")
-    return db_star
+@app.post("/movies/add_actor", response_model=schemas.MovieDetail)
+def add_movie_actor(mid: int, sid: int, db: Session = Depends(get_db)):
+    db_movie = crud.add_movie_actor(db=db, movie_id=mid, actor_id=sid)
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie or Star not found")
+    return db_movie
+
+########## App PUT
+
+@app.put("/movies/director", response_model=schemas.MovieDetail)
+def update_movie_director(mid: int, sid: int, db: Session = Depends(get_db)):
+    db_movie = crud.update_movie_director(db=db, movie_id=mid, director_id=sid)
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie or Star not found")
+    return db_movie
+
+
+@app.put("/movies/update_actors", response_model=schemas.MovieDetail)
+def update_movie_actors(mid: int, sids: List[int], db: Session = Depends(get_db)):
+    db_movie = crud.update_movie_actor(db=db, movie_id=mid, actor_id=sids)
+    if db_movie is None:
+        raise HTTPException(status_code=404, detail="Movie or Star not found")
+    return db_movie
+    
